74dbad35902b469f90a7c36cac41c335d8d7e389
TANABE Ken-ichi <nabeken@tknetworks.org>
2014-01-25 17:04:44 +0900

    cronapt: Set default to upgrade

--- attributes/cronapt.rb
+++ attributes/cronapt.rb
@@ -13,5 +13,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-default['debian']['cronapt']['mailon'] = 'output'
+default['debian']['cronapt']['mailon'] = 'upgrade'
 default['debian']['cronapt']['mailto'] = 'root'
