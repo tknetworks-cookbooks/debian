e0d6ed4ecb99ebb94d72e5900c039453d8fed692
TANABE Ken-ichi <nabeken@tknetworks.org>
2014-01-25 17:00:22 +0900

    Add debian::cronapt

--- metadata.rb
+++ metadata.rb
@@ -3,8 +3,12 @@ maintainer_email "nabeken@tknetworks.org"
 license          "Apache 2.0"
 description      "Installs/Configures debian"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.0.2"
 name             "debian"
+
+%w{apt}.each do |c|
+  depends c
+end
 
 %w{debian ubuntu}.each do |os|
   supports          os
