30ef3855f5a8d5316e07b6b2d482af64f97540ab
TANABE Ken-ichi <nabeken@tknetworks.org>
2015-04-25 15:49:52 +0900

    rename debian to tknetworks_debian

--- metadata.rb
+++ metadata.rb
@@ -3,8 +3,8 @@ maintainer_email "nabeken@tknetworks.org"
 license          "Apache 2.0"
 description      "Installs/Configures debian"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.2"
-name             "debian"
+version          "0.0.3"
+name             "tknetworks_debian"
 
 %w{apt}.each do |c|
   depends c
